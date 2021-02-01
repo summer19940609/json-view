8da61c3164de7ad2d1fdb1629c908e5e22fd813d
null <summer19940609@gmail.com>
2021-02-01 14:36:55 +0800

    Create json-view.js

---
+++ json-view.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Json.cn净化
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  json.cn 纯净版
+// @author       aries.zhou
+// @match        https://www.json.cn/**
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    // 隐藏垃圾内容
+    $('header').append("<style> .header,.width-out {display:none;} .container {display:none;} .blog-list {display:none;} .footer-nav-list {display:none;} .content {display:none;} div.tool li{display:none;} .editor-con { height: 100vh !important;} </style>");
+    // 获取URL里的json
+    const json = getQueryString('json')
+    // 填入文本，并触发
+    $('#json-src').val(json);
+    $('#json-src').keyup();
+
+    // 解析URL参数
+    function getQueryString(name) {
+        let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        let r = window.location.search.substr(1).match(reg);
+        if (r != null) {
+            return decodeURIComponent(r[2]);
+        };
+        return null;
+    }
+
+})();
